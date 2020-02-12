a1c0bb94d2f9b764bdf6b5759cae70efe367f785
marceloh1987 <marceloh1987@gmail.com>
2020-02-12 02:54:28 -0300

    Remodelacion

---
+++ src/components/MainContent/MainContent.js
@@ -0,0 +1,15 @@
+import React, { Component } from  'react';
+
+class MainContent extends Component {
+    render() {
+        return(
+            <div><h1>MainContent</h1></div>
+        )
+    }
+}
+
+export default MainContent;
+
+
+
+
