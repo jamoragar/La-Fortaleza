d06aa5aff6e3700f21c5724fd4e791f1dab37c3e
jamoragar <javier.moragarojas@gmail.com>
2020-03-24 21:47:58 -0300

    NEW - integración Firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,14 +3,14 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDcLbqT0CK9_XaYr3nakqel1nmbbLpuwhk",
-    authDomain: "la-fortaleza-995f7.firebaseapp.com",
-    databaseURL: "https://la-fortaleza-995f7.firebaseio.com",
-    projectId: "la-fortaleza-995f7",
-    storageBucket: "la-fortaleza-995f7.appspot.com",
-    messagingSenderId: "8687471788",
-    appId: "1:8687471788:web:84f40095ac18f6d5efa309",
-    measurementId: "G-MG3P7T774L"
+    aapiKey: "AIzaSyB3RDp5bPyATXDkubYncRoby1uZkndZyM8",
+    authDomain: "la-fortaleza-18fbf.firebaseapp.com",
+    databaseURL: "https://la-fortaleza-18fbf.firebaseio.com",
+    projectId: "la-fortaleza-18fbf",
+    storageBucket: "la-fortaleza-18fbf.appspot.com",
+    messagingSenderId: "418002920018",
+    appId: "1:418002920018:web:0331aa18aff2a8ee77358b",
+    measurementId: "G-4GGWZ3M72X"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
