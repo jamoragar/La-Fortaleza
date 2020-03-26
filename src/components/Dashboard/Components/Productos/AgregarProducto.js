4f47d6ac73a82dcd0b2289e9391a6123581972c2
jamoragar <javier.moragarojas@gmail.com>
2020-03-26 02:05:02 -0300

    FIX - Se corrige bug en Agregar Producto y se agrega la subcategoría, según la categoría seleccionada

--- src/components/Dashboard/Components/Productos/AgregarProducto.js
+++ src/components/Dashboard/Components/Productos/AgregarProducto.js
@@ -4,6 +4,7 @@ import firebase from '../../../../config/firebase';
 
 const AgregarProducto = (props) => {
     const [fbCategoria, setFbCategoria] = useState(null);
+    const [subCat, setSubCat] = useState(null);
     const fbDbCategory = firebase.database().ref('/Category');
     let descripciones = []
 
@@ -17,6 +18,14 @@ const AgregarProducto = (props) => {
             setFbCategoria(descripciones);
         })
     }, []);
+
+    const handleSubCategory = (categoria) => {
+        fbDbCategory.orderByChild('description/').equalTo(`${categoria}`).on('value', snapshot => {
+            snapshot.forEach((child) => {
+                setSubCat(child.val().subCat)
+            })
+        });
+    }
 
     return (
         <Modal {...props} style={{background:'none'}}>
@@ -37,14 +46,30 @@ const AgregarProducto = (props) => {
                             <Form.Label>Descripción:</Form.Label>
                             <Form.Control as="textarea" rows="3" placeholder='Ingrese la descripción del producto.' />
                         </Form.Group>
-                        <Form.Group controlId="exampleForm.ControlSelect1">
+                        <Form.Group controlId="formCategory">
                             <Form.Label>Categoría:</Form.Label>
-                            <Form.Control as="select">
+                            <Form.Control name='selectCategory' as="select" onChange={(categoria) => handleSubCategory(categoria.target.value)}>
+                                <option value='no_select' key='alfa'>Seleccione una categoría</option>
                                 {fbCategoria.map((categoria, i) => {
                                     return <option value={categoria} key={i}>{categoria}</option>
                                 })}
                             </Form.Control>
                         </Form.Group>
+                        {
+                            subCat ? 
+                            <Form.Group controlId="formCategory">
+                                <Form.Label>Sub-Categoría:</Form.Label>
+                                <Form.Control name='selectSubCategory' as="select">
+                                    {
+                                        Object.entries(subCat).map(([abreviacion, nombre], i) => {
+                                            return <option value={nombre} key={i}>{nombre}</option>
+                                        })
+                                    }
+                                </Form.Control>
+                            </Form.Group>
+                            :
+                            null
+                        }
                         <Form.Group controlId="formPriceProducts">
                             <Form.Label>Precio:</Form.Label>
                             <Form.Control type="number" placeholder='Ingrese el valor del producto.' />
