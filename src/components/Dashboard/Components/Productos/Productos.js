1fcaa646037df30023301da4b879de97ee967f87
jamoragar <javier.moragarojas@gmail.com>
2020-03-27 03:39:37 -0300

    NEW - Tabla de productos, recoge y muestra información desde bd de firebase

--- src/components/Dashboard/Components/Productos/Productos.js
+++ src/components/Dashboard/Components/Productos/Productos.js
@@ -1,60 +1,71 @@
-import React , {useState} from 'react';
+import React , {useState, useEffect} from 'react';
+import firebase from '../../../../config/firebase';
 import {Table, Button} from 'react-bootstrap';
 import AgregarProducto from './AgregarProducto';
 
 const Productos = () => {
     const [showModal, setShowModal] = useState(false);
+    const [productos, setProductos] = useState(null)
     const handleShow = () => setShowModal(true);
+    let productosToArray = [];
+    useEffect(() => {
+        firebase.database().ref('/Productos').on('value', snapshot => {
+            setProductos(snapshot.val());
+        });
+    }, []);
 
+    if(productos){
+        Object.keys(productos).map((key, i) => {
+            productosToArray[i] = productos[key]
+        })
+        console.log(productosToArray)
         return (
             <div>
-                <Button style={{float:'right'}} onClick={handleShow} variant="primary">Agregar Producto</Button>
+                <Button style={{float:'right'}} onClick={handleShow} variant="primary">
+                    <i className="fas fa-tag fa-fw" />
+                    Agregar Producto
+                </Button>
                 <AgregarProducto show={showModal} onHide={() => setShowModal(false)} />
-                <br /><br />
+                <br/><br/><br/>
                 <Table responsive>
                     <thead>
                         <tr>
                         <th>#</th>
-                        <th>Table heading</th>
-                        <th>Table heading</th>
-                        <th>Table heading</th>
-                        <th>Table heading</th>
-                        <th>Table heading</th>
-                        <th>Table heading</th>
+                        <th>Foto</th>
+                        <th>Nombre</th>
+                        <th>Precio</th>
+                        <th>Categoría</th>
+                        <th></th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                        <td>1</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        </tr>
-                        <tr>
-                        <td>2</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        </tr>
-                        <tr>
-                        <td>3</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        </tr>
+                        {
+                            productosToArray.map((producto, i) => {
+                                return(
+                                    <tr key={i}>
+                                        <td>{i}</td>
+                                        <td><img src={producto.img[0]} width='80' alt="..."/></td>
+                                        <td>{producto.nombre}</td>
+                                        <td>{producto.precio}</td>
+                                        <td>{producto.categoria}</td>
+                                        <td>
+                                            <a href='#' className="text-success"><i className="fas fa-fw fa-search" /> </a> 
+                                            <a href='#' className="text-primary"><i className="fa fa-fw fa-edit" /> </a> 
+                                            <a href='#' className="text-danger"><i className="fa fa-fw fa-trash" /></a>
+                                        </td>   
+                                    </tr>
+                                )
+                            })
+                        }
                     </tbody>
                 </Table>
             </div>
         )
+    }else{
+        return(
+            <h3>Cargando...</h3>
+        )
     }
+}
 
 export default Productos;
