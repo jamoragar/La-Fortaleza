62474337876120ec964e470232692b6f01b87313
Marcelo Hernández <marceloh1987@gmail.com>
2020-03-30 05:26:49 -0300

    Imagenes de carousel y side photos personalizadas

--- src/components/Layouts/Carousel/Carousel.js
+++ src/components/Layouts/Carousel/Carousel.js
@@ -12,44 +12,21 @@ const CarouselContainer = () => {
             <Carousel.Item>
               <img
                 className="d-block w-100"
-                src="https://firebasestorage.googleapis.com/v0/b/la-fortaleza-18fbf.appspot.com/o/Slider%2Fgrid3.jpg?alt=media&token=8a6651b2-98d5-4175-adaa-283e1bf98c00"
+                src="https://firebasestorage.googleapis.com/v0/b/la-fortaleza-18fbf.appspot.com/o/Slider%2FSlider-Pandemic.png?alt=media&token=c1edaca8-b131-4dd7-88ba-f64d8e2e2472"
                 alt="First slide"
                 height="475"
               />
               <Carousel.Caption>
-                <h3>Takenoko</h3>
-                <p>
-                  acompaña a nuestro simpático pandita y a nuestro laborioso
-                  jardinero en las aventuras de la exótica China imperial
-                </p>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
               <img
                 className="d-block w-100"
-                src="https://firebasestorage.googleapis.com/v0/b/la-fortaleza-18fbf.appspot.com/o/Slider%2Fgrid5.jpg?alt=media&token=08bf8c34-4c99-4306-b3cd-d796e5f32c77"
-                alt="Third slide"
-                height="475"
-              />
-
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="img/grid5.jpg"
+                src="https://firebasestorage.googleapis.com/v0/b/la-fortaleza-18fbf.appspot.com/o/Slider%2FSliderUgears.png?alt=media&token=e51628d1-6b6c-43cc-9c21-692b8ce31af9"
                 alt="Third slide"
                 height="475"
               />
               <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>
-                  Praesent commodo cursus magna, vel scelerisque nisl
-                  consectetur.
-                </p>
               </Carousel.Caption>
             </Carousel.Item>
           </Carousel>
@@ -58,14 +35,14 @@ const CarouselContainer = () => {
         <Col lg={4} md={4} sm={6} xs={12}>
           <img
             className="d-block w-100 "
-            src="img/grid3.jpg"
+            src="https://firebasestorage.googleapis.com/v0/b/la-fortaleza-18fbf.appspot.com/o/Slider%2Fgrid1.png?alt=media&token=284c98da-0f09-4756-b5eb-bb73d6bc39d5"
             alt="First slide"
             height="225"
           />
           <br />
           <img
             className="d-block w-100 "
-            src="img/grid3.jpg"
+            src="https://firebasestorage.googleapis.com/v0/b/la-fortaleza-18fbf.appspot.com/o/Slider%2Fgrid2.png?alt=media&token=f78c89ed-1ec5-405e-bf71-eef8f3ad593f"
             alt="First slide"
             height="225"
           />
