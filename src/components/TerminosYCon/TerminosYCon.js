d400bcc590975f9d6b8a4e544d93e027094beb67
Marcelo Hernández <marceloh1987@gmail.com>
2020-04-19 21:35:52 -0300

    se modifica nombre de componente en terminos y servicios

---
+++ src/components/TerminosYCon/TerminosYCon.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import './TerminosYCon.scss';
+
+const Terms = () => {
+    return (
+        <div className="container-terminos" fluid="true">
+            <h3 className="terminos-h3 mt-5 mb-5">
+                Términos y Condiciones de Uso
+            </h3>
+            <p className="terminos m-3">
+                1. Tienda y organizadora de eventos la Fortaleza Limitada en adelante La Fortaleza, NO promociona material ilícito, inmoral, obsceno o nocivo; NO promociona ningún tipo de discriminación; NO remite "bulk" (correo masivo), "junk" (correo basura), spam-email (correo no deseado) ni boletines que no hayan sido solicitados; NO incumple ningún derecho de propiedad intelectual o industrial de terceras compañías.
+
+            </p>
+            <p className="terminos m-3" >
+                2. El uso de los servicios de La Fortaleza por menores de edad supone el consentimiento de los padres.
+            </p>
+            <p className="terminos m-3">
+                3. El pago es realizado únicamente a través de depósito bancario, transferencia o pago con tarjeta cuyos datos son mostrados en el panel de pago cuando se está ejecutando el pedido, y los datos del boucher son enviados a través del panel del Cliente. Por lo tanto La Fortaleza supone que dicha transacción (depósito, transferencia, tarjeta bancaria o crédito) es realizada por el usuario que la envía.
+
+            </p>
+            <p className="terminos m-3">
+                4. En el caso de los depósitos bancarios o transferencias que el usuario realiza no aparece en la cuenta que La Fortaleza admite, no se podrá procesar el pedido y La Fortaleza no se hará responsable del destino del dinero depositado o transferido. El usuario una vez realizado el depósito debe enviar una copia del comprobante a lafortalezapuq@gmail.com
+            </p>
+            <p className="terminos m-3">
+                5. Se entenderá que el usuario acepta las condiciones establecidas al registrarse en nuestro sitio.
+            </p>
+            <p className="terminos m-3">
+                6. La finalidad principal del tratamiento de sus datos personales en La Fortaleza es la venta y envío de los productos de nuestra web. Los datos serán conservados para su tratamiento en nuestros sistemas mientras exista una relación establecida, o bien por motivos legítimos imperiosos, o el ejercicio o la defensa de posibles reclamaciones. Una vez terminados los plazos legales de almacenamiento en nuestros sistemas, sus datos podrán ser eliminados.  Adicionalmente, sus datos podrán ser tratados para el envío de información comercial, sobre nuestros productos, ofertas y novedades, en base a los consentimientos por usted prestados.
+            </p>
+            <p className="terminos m-3 mb-5">
+                7. En caso de que el Usuario requiera factura por el total de sus compras y no lo solicite al ingresar al portal web de la Fortaleza, podrá solicitarla al correo lafortalezapuq@gmail.com, enviado para tales efectos el número de su orden, nombre o razón social, rut y domicilio fiscal completo. Las facturas deben ser pagadas con transferencia bancaria o dinero efectivo no siendo aceptado otro tipo de forma de pago.
+
+            </p>
+            <p className="terminos m-3 mb-5">
+                8. Los precios en esta página no reflejan necesariamente los precios de los productos en nuestra tienda física, es probable que acá algunos ítems se encuentren en promoción u oferta exclusiva para Internet.
+
+            </p>
+            <p className="terminos m-3 mb-5">
+                9. Verifique periódicamente si hubo algún cambio en esta página, pues el uso del sitio implica el conocimiento y aceptación de nuestra política de uso.
+
+
+            </p>
+            <h3 className="terminos-h3 mt-5 mb-5">
+                Política de devoluciones y/o problemas con componentes.
+            </h3>
+            <p className="terminos m-3 mb-5">
+                Todo producto sellado contará con la garantía establecida por ley (3 meses) siempre y cuando se encuentre en las siguientes condiciones: a) Debe conservar su empaque de origen, incluyendo el plástico protector, b) En cliente deberá hacerse cargo de los gastos de envío para su devolución, c) El producto a devolver debe estar correctamente embalado en el momento en que La Fortaleza lo reciba.
+
+            </p>
+            <p className="terminos m-3 mb-5">
+                De faltar algún componente al abrir el juego, se deberá escribir a lafortalezapuq@gmail.com con fotos del producto para coordinar una solución al caso.
+            </p>
+            <h4 className="terminos-h3 mt-5 mb-5">
+                Despachos
+            </h4>
+            <p className="terminos m-3 mb-5">
+                La Fortaleza se compromete a entregar los pedidos de sus clientes en un plazo no mayor a 48 horas hábiles sus pedidos dentro del área urbana de Punta Arenas, sin cargo para sus clientes.  Esto aplica para compras superiores a 30.000.
+            </p>
+            <p className="terminos m-3 mb-5">
+                Para la región de Magallanes y el resto de Chile, el envío debe ser coordinado.  Para ello agradecemos enviar un correo a lafortalezapuq@gmail.com.
+            </p>
+            <h4 className="terminos-h3 mt-5 mb-5">
+                Encargos y preventas
+            </h4>
+            <p className="terminos m-3 mb-5">
+                Los plazos estimados para preventas y encargos, considerando la realidad geográfica y de transporte de nuestra región, tienen un margen de llegada en la fecha comprometida inicialmente. Si bien comunicaremos oportunamente vía correo electrónico cualquier actualización respecto a la llegada de los productos, no se aceptarán devoluciones solicitadas con motivo de un retraso menor a:
+            </p>
+            <p className="terminos m-3 mb-5">
+                10 días hábiles en el caso de preventas
+            </p>
+            <p className="terminos m-3 mb-5">
+                5 días hábiles en el caso de encargos.
+            </p>
+            <p className="terminos m-3 mb-5">
+                Respecto a la fecha inicial comprometida. Estos plazos no aplicarán en casos que escapen de nuestro control (por ejemplo, contingencia nacional, anomalía en el funcionamiento de Couriers, retrasos mundiales o nacionales en la entrega de un determinado título, etc.).
+            </p>
+
+        </div>
+    )
+}
+
+export default Terms;
