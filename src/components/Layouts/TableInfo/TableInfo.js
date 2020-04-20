0b0198eb42d026fb63ca6779d367d50a6cf6c834
Marcelo Hernández <marceloh1987@gmail.com>
2020-04-19 21:33:17 -0300

    banners, tableinfo, termninos y condiciones actualizados.

--- src/components/Layouts/TableInfo/TableInfo.js
+++ src/components/Layouts/TableInfo/TableInfo.js
@@ -11,7 +11,7 @@ export default function TableInfo() {
                                 <th scope="row"><span style={{ fontSize: "4em", color: "#606060" }}><i className="fas fa-dolly"></i></span></th>
                                 <td className="mt-4">
                                     <p className="h5-responsive"><strong>Entrega gratuita</strong></p>
-                                    <p className="h6-responsive text-muted">Por compras superiores a $30.000 pesos.</p>
+                                    <p className="h6-responsive text-muted">Por compras superiores a $30.000 pesos. (Dentro de la zona urbana de Punta Arenas)</p>
                                 </td>
                             </tr>
                         </tbody>
@@ -24,7 +24,7 @@ export default function TableInfo() {
                                 <th scope="row"><span style={{ fontSize: "4em", color: "#606060" }}><i className="fas fa-credit-card"></i></span></th>
                                 <td>
                                     <p className="h5-responsive"><strong>Metodos de pago seguro</strong></p>
-                                    <p className="h6-responsive text-muted">Credito, debito, transferencia y efectivo.</p>
+                                    <p className="h6-responsive text-muted">Crédito, débito, transferencia y efectivo.</p>
                                 </td>
                             </tr>
                         </tbody>
@@ -37,7 +37,7 @@ export default function TableInfo() {
                                 <th scope="row"><span style={{ fontSize: "4em", color: "#606060" }}><i className="fas fa-gift"></i></span></th>
                                 <td className="mt-4">
                                     <p className="h5-responsive"><strong>Servicios Especiales</strong></p>
-                                    <p className="h6-responsive text-muted">Envolvemos tu compra para regalo.</p>
+                                    <p className="h6-responsive text-muted">Envolvemos tu compra para regalo. (cargo adicional)</p>
                                 </td>
                             </tr>
                         </tbody>
@@ -50,7 +50,7 @@ export default function TableInfo() {
                                 <th scope="row"><span style={{ fontSize: "4em", color: "#606060" }}><i className="fas fa-tags"></i></span></th>
                                 <td className="mt-4">
                                     <p className="h5-responsive"><strong>Ofertas</strong></p>
-                                    <p className="h6-responsive text-muted">Los mejores descuentos en articulos seleccionados.</p>
+                                    <p className="h6-responsive text-muted">Los mejores descuentos en artículos seleccionados.</p>
                                 </td>
                             </tr>
                         </tbody>
