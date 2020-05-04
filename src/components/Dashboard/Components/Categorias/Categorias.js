41f002b2da5581ad3dcaa5e81bf5d4bf2666051b
Marcelo Hernández <marceloh1987@gmail.com>
2020-05-04 08:40:02 -0300

    Mantenedores
    - Mantenedor de slider finalizado.
    - mantenedor de categorias: se agrega listado desplegable para cada categoria mostrando sus subcategorias, eliminar categorias y subcategorias funcionando.
    - agregar categorias en proceso.

--- src/components/Dashboard/Components/Categorias/Categorias.js
+++ src/components/Dashboard/Components/Categorias/Categorias.js
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { Spinner, Button } from 'react-bootstrap';
 import firebase from '../../../../config/firebase';
 import DataTable from 'react-data-table-component';
-import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { OverlayTrigger, Tooltip, Card, Accordion, Row, Col } from 'react-bootstrap';
 import AgregarCategorias from './AgregarCategorias';
 import AgregarSubCategorias from './AgregarSubCategorias';
+import SubCategorias from './SubCategorias';
 
 const Categorias = () => {
     const columns = [
@@ -12,6 +13,13 @@ const Categorias = () => {
             name: 'Foto',
             cell: row => <a href={row.banner} target="blank"><img src={row.banner} width='75' alt="..." /></a>,
             grow: 1,
+        },
+        {
+            name: 'Id',
+            selector: 'id',
+            sortable: true,
+            grow: 2,
+
         },
         {
             name: 'Nombre',
@@ -39,7 +47,7 @@ const Categorias = () => {
                                     <Tooltip id={`tooltip-bottom`}><strong>Eliminar</strong></Tooltip>
                                 }
                             >
-                                <div style={{ cursor: 'pointer' }} onClick={() => /*deleteCategory(data)*/ console.log(`eliminando ${data.id}`)} className="text-danger"><i className="fa fa-fw fa-trash fa-lg" style={{ height: '20px' }} /></div>
+                                <div style={{ cursor: 'pointer' }} onClick={() => (opcion(true)) ? deleteCategory(data) : null} className="text-danger"><i className="fa fa-fw fa-trash fa-lg" style={{ height: '20px' }} /></div>
                             </OverlayTrigger>
                         </div>
                     </div>
@@ -49,10 +57,13 @@ const Categorias = () => {
         }
     ];
 
-    const [showModal, setShowModal] = useState(false);
+    const [showModal1, setShowModal1] = useState(false);
+    const [showModal2, setShowModal2] = useState(false);
     const [categorias, setCategorias] = useState([])
     let categoriasToArray = [];
-    const handleShow = () => setShowModal(true);
+    const handleShow1 = () => setShowModal1(true);
+    const handleShow2 = () => setShowModal2(true);
+    const opcion = () => window.confirm('Esta seguro que desea eliminar esta categoria?');
 
     useEffect(() => {
         firebase.database().ref('/Category').on('value', snapshot => {
@@ -76,11 +87,15 @@ const Categorias = () => {
         }
     }
 
+
     if (categorias || categorias.length >= 0) {
 
         Object.keys(categorias).forEach((key, i) => {
             categoriasToArray[i] = categorias[key]
         });
+
+        const paginationOptions = { rowsPerPageText: 'Filas por página', rangeSeparatorText: 'de', selectAllRowsItem: true, selectAllRowsItemText: 'Todos' };
+
         return (
             <div>
                 <div className="row" >
@@ -88,22 +103,36 @@ const Categorias = () => {
                         <h1>Mantenedor De Categorias</h1>
                     </div>
                 </div>
-                <Button style={{ float: 'right' }} onClick={handleShow} variant="primary">
-                    <i className="fas fa-tag fa-fw" />
-                    Agregar Categoria
-                </Button>
-                <AgregarCategorias show={showModal} onHide={() => setShowModal(false)} />
-                <Button style={{ float: 'right' }} onClick={handleShow} variant="primary">
-                    <i className="fas fa-tag fa-fw" />
-                    Agregar Subcategoria
-                </Button>
-                <AgregarSubCategorias show={showModal} onHide={() => setShowModal(false)} />
-                <DataTable
-                    title="Categorias"
-                    columns={columns}
-                    data={categoriasToArray}
-                    persistTableHead
-                />
+                <div className="row">
+                    <div className="col text-center">
+                        <Button style={{ float: 'right', marginRight: '1rem', marginTop: '3rem', marginBottom: '2rem', width: '100' }} onClick={handleShow2} variant="primary">
+                            <i className="fas fa-tag fa-fw" />
+                                Agregar Categoria
+                        </Button>
+                        <AgregarCategorias show={showModal2} onHide={() => setShowModal2(false)} />
+                        <Button style={{ float: 'right', marginRight: '1rem', marginTop: '3rem', marginBottom: '2rem', marginLeft: '2rem' }} onClick={handleShow1} variant="primary">
+                            <i className="fas fa-tag fa-fw" />
+                                Agregar Subcategoria
+                        </Button>
+                        <AgregarSubCategorias show={showModal1} onHide={() => setShowModal1(false)} />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        < DataTable
+                            columns={columns}
+                            data={categoriasToArray}
+                            persistTableHead
+                            fixedHeader
+                            fixedHeaderScrollHeight="500px"
+                            noHeader
+                            pagination
+                            paginationComponentOptions={paginationOptions}
+                            expandableRows
+                            expandableRowsComponent={<SubCategorias catData={categoriasToArray.id} />}
+                        />
+                    </div>
+                </div>
             </div>
         );
     }
--- src/components/Dashboard/Components/Slider/EditarButtons.js
+++ src/components/Dashboard/Components/Slider/EditarButtons.js
@@ -1,47 +1,145 @@
-import React, { useState, useEffect } from 'react';
-import { Spinner, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Spinner, Button, ProgressBar, Alert, Form } from 'react-bootstrap';
 import { useParams, Link } from 'react-router-dom';
 import firebase from '../../../../config/firebase';
+import sliderStyles from './Slider.module.scss';
 
 const EditarButtons = () => {
-    const [buttons, setButtons] = useState(null);
+    const [image, setImage] = useState(null);
+    const [files, setFiles] = useState([])
     let { id } = useParams();
+    let { uid } = useParams();
+    const [alertShow, setAlertShow] = useState(false);
+    const [buttonAceptarText, setButtonAceptarText] = useState(true);
+    const [progress, setProgress] = useState(0);
+    let images = [];
+    let fileArray = [];
 
-    useEffect(() => {
-        firebase.database().ref(`/Slider/Buttons/${id}`).on('value', snapshot => {
-            setButtons(snapshot.val());
-        })
-    }, [])
+    const orientImage = async ({ target }) => {
+        images.push(target.files)
+        setFiles(target.files)
+        for (let i = 0; i < images[0].length; i++) {
+            fileArray.push(URL.createObjectURL(images[0][i]));
+        }
+        setImage(await fileArray);
+    }
+    const deleteImage = (e, name) => {
+        e.preventDefault();
+        setImage(image.filter((image) => image !== name))
 
-    if (id && buttons) {
-        const { img, id } = buttons;
-        return (
-            <div>
-                <h3>Producto ID: {id}</h3>
-                <img alt="..." src={img} />
-                <Link to='/Dashboard/:uid/Slider' >
+    }
+
+    const handleReset = () => {
+        document.getElementById('myForm').reset();
+        setImage(null);
+    }
+
+    const submitSlider = (e) => {
+        e.preventDefault();
+        const promises = [];
+        const FbDownloadURL = [];
+        if (files.length > 0 && id) {
+            setButtonAceptarText(false)
+            for (let i = 0; i < files.length; i++) {
+                const uploadTask = firebase.storage().ref().child(`Slider/Buttons/${id.trim()}/Button${id.trim()}`).put(files[i])
+                promises.push(uploadTask);
+                uploadTask.on(
+                    firebase.storage.TaskEvent.STATE_CHANGED,
+                    snapshot => {
+                        const progress = ((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+                        if (snapshot.state === firebase.storage.TaskState.RUNNING) {
+                            setProgress(progress);
+                        }
+                    },
+                    error => console.log(error.code),
+                    async () => {
+                        const downloadURL = await uploadTask.snapshot.ref.getDownloadURL();
+                        FbDownloadURL.push(downloadURL);
+                        if (FbDownloadURL.length === files.length) {
+                            firebase.database().ref().child(`/Slider/Buttons/button${id}/`).update({
+                                id: id,
+                                img: FbDownloadURL.map((img) => {
+                                    return img
+                                }),
+                            })
+                        }
+                    }
+                );
+            }
+            Promise.all(promises)
+                .then(() => {
+                    setButtonAceptarText(true);
+                    setAlertShow(true);
+                    setTimeout(() => {
+                        setAlertShow(false);
+                        handleReset();
+                    }, 2173);
+                })
+                .catch(err => console.log(err.code));
+        }
+        else {
+            alert('Debe completar todos los campos y agregar al menos una imágen.');
+        }
+    }
+    return (
+        <div>
+            <Form onSubmit={submitSlider} id='myForm'>
+                <Form.Group controlId='formUploadImages'>
+                    <Form.Label className={sliderStyles.fltext}>
+                        Selecciona una imagen para reemplazar la imagen {id} del Boton:
+                    </Form.Label>
+                    <div className={'custom-file'} style={{ marginBottom: '12px' }}>
+                        <input type="file" className={'custom-file-input'} id="customFile" onChange={orientImage} accept="image/*" multiple />
+                        <label className="custom-file-label" htmlFor="customFile">Buscar Imágen(es)</label>
+                    </div>
+                    {
+                        image ? (
+                            <div>
+                                <div className={sliderStyles.containerImg}>
+                                    {(image || []).map((url, i) => {
+                                        return (
+                                            <div key={i}>
+                                                <div className={sliderStyles.boxImg} key={i}>
+                                                    <img src={url} alt="..." />
+                                                    <div className={sliderStyles.optionImg}>
+                                                        <button onClick={(e) => deleteImage(e, url)} className={sliderStyles.deleteImg}>
+                                                            <svg className={sliderStyles.svgX} focusable="false" viewBox="0 0 24 24" aria-hidden="true" role="presentation"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></svg>
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                                <ProgressBar variant="success" now={progress} />
+                            </div>
+                        )
+                            :
+                            null}
+                </Form.Group>
+
+                <Button type='submit' variant="success" block style={{ margin: '20px 0 0 0' }}>
+                    {buttonAceptarText ? (<><i className="far fa-save fa-fw" />Aceptar</>) : (<Spinner animation="border" />)}
+                </Button>
+                {' '}
+                <Button variant="outline-secondary" onClick={handleReset} block>
+                    <i className="fas fa-undo fa-fw" />
+                            Reiniciar
+                        </Button>
+                <br />
+                <Alert show={alertShow} variant={'success'} onClose={() => setAlertShow(false)} dismissible>
+                    Imagen Subida con éxito!
+                        </Alert>
+
+                <Link to={`/Dashboard/${uid}/Slider`} >
                     <Button variant="outline-primary" block>
                         <i className="fas fa-undo fa-fw" />
                             Volver
                     </Button>
                 </Link>
-            </div>
-        );
-    } else {
-        return (
-            <>
-                <Spinner animation="grow" variant="primary" />
-                <Spinner animation="grow" variant="secondary" />
-                <Spinner animation="grow" variant="success" />
-                <Spinner animation="grow" variant="danger" />
-                <Spinner animation="grow" variant="warning" />
-                <Spinner animation="grow" variant="info" />
-                <Spinner animation="grow" variant="light" />
-                <Spinner animation="grow" variant="dark" />
-            </>
-        )
-    }
+            </Form>
+        </div>
+    )
 }
-
 
 export default EditarButtons;
