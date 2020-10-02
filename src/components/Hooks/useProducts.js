8c54c82eaa184c9b0d55302f0249c3cfb516ecc1
jamoragar <javier.moragarojas@gmail.com>
2020-10-02 00:23:59 -0300

    fix unsubscribe

--- src/components/Hooks/useProducts.js
+++ src/components/Hooks/useProducts.js
@@ -8,7 +8,7 @@ export function useProducts() {
     const [fbData, setFbData] = useState(null);
 
     useEffect(() => {
-        firebase.database().ref('/Productos').orderByChild('fecha_creacion').limitToLast(240).once('value').then(snapshot => {
+        const unsubscribe = firebase.database().ref('/Productos').orderByChild('fecha_creacion').limitToLast(240).once('value').then(snapshot => {
             console.log(snapshot.val())
             setFbData(snapshot.val());
         });
