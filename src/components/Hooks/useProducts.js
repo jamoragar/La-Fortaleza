0997b23504e003fce7fe14ec3b294bb6eb13c775
jamoragar <javier.moragarojas@gmail.com>
2020-10-05 12:13:54 -0300

    cambios para actualizar fecha

--- src/components/Hooks/useProducts.js
+++ src/components/Hooks/useProducts.js
@@ -6,30 +6,60 @@ import fs from 'browserify-fs';
 
 export function useProducts() {
     const [fbData, setFbData] = useState(null);
+    const [fbCategoria, setFbCategoria] = useState(null);
+    const fbDbCategory = firebase.database().ref('/Category');
+    let productos = [];
 
     useEffect(() => {
-        console.log(Date.now())
-        const unsubscribe = firebase.database().ref('/Productos').orderByChild('fecha_creacion').limitToFirst(10).on('value', snapshot => {
+        // fbDbCategory.orderByChild('description').on('value', (snapshot) => {
+        //     // console.log('nombre del nodo: ' + snapshot.key + ' description: ' + snapshot.val().description);
+        //     snapshot.forEach(child => {
+        //         // console.log(child.val().description)
+        //         firebase.database().ref('/Productos').orderByChild('categoria').equalTo(child.val().description).limitToLast(18).once('value').then(snapshot => {
+        //             productos.push(snapshot.val())
+        //             console.log(productos)
+        //             setFbData(productos)
+        //         });
+        //     });
+        // });
+        
+        const unsubscribe = firebase.database().ref('/Productos').orderByChild('fecha_creacion').limitToLast(240).once('value').then(snapshot => {
             console.log(snapshot.val())
             setFbData(snapshot.val());
         });
-        return (() => unsubscribe());
+
+        return (() => unsubscribe())
     }, []);
+    
     // if(fbData){
-    //     let time;
-    //     Object.keys(fbData).map(key => {
-    //         time = moment(fbData[key].fecha_creacion, 'DD-MM-YYYY h:mm:ss a').format('X');
-    //         fbData[key].fecha_creacion = parseInt(time);
-    //     });
-
-    //     console.log(fbData)
-
-    //     firebase.database().ref().child(`/Productos`).set(fbData)
-    // }
-
+    //     console.log(fbData.length)
+    //     return {
+    //         fbData,
+    //         setFbData
+    //     }
+    // }else{
+    //     return(
+    //         [],
+    //         setFbData
+    //     )
     return {
         fbData,
         setFbData
     }
-}
+    }
+    //     let time;
+    //     Object.keys(fbData).map(key => {
+    //         time = moment(fbData[key].fecha_creacion, 'DD-MM-YYYY h:mm:ss a').format('X');
+    //         fbData[key].fecha_creacion = time === 'Invalid date' ? fbData[key].fecha_creacion : parseInt(time)
+
+    //     });
+
+    //     console.log(fbData)
+        
+    //     setTimeout(() => {
+    //         firebase.database().ref('/Productos').set(fbData);
+    //     }, 6000)
+
+    // }
+
 //nuevo
